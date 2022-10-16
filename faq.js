e9e1d0cb00e3964ba47e91d9e281763ec48ba03d
yashPundhir <yash.pundhir.prof@gmail.com>
2022-10-16 22:12:45 +0530

    all the required functionality has been added to the JS file

--- faq.js
+++ faq.js
@@ -1,3 +1,4 @@
+//main object containing all the required info
 const faqData = [
 	{
 		id: 1,
@@ -26,37 +27,51 @@ function showFaq() {}
 function createFaq() {}
 
 function btnStatusUpdate() {}
-// console.log(faqData[0]);
-// console.log(faqData[1]);
-// console.log(faqData[2]);
-let faq = document.createElement("div");
-faq.className = "faq";
-document.querySelector(".accordian_body").appendChild(faq);
-// ****************************
-let faqHeader = document.createElement("div");
-faqHeader.className = "faq_header";
-document.querySelector(".accordian_body .faq").appendChild(faqHeader);
-// ****************************
-let faqQues = document.createElement("h3");
-document.querySelector(".accordian_body .faq .faq_header").appendChild(faqQues);
-faqQues.textContent = faqData[0].question;
-// ****************************
-let faqBtn = document.createElement("button");
-faqBtn.className = "show_btn";
-document.querySelector(".accordian_body .faq .faq_header").appendChild(faqBtn);
-faqBtn.textContent = "+";
-// ****************************
-let faqAns = document.createElement("p");
-document.querySelector(".accordian_body .faq").appendChild(faqAns);
-faqAns.textContent = faqData[0].answer;
-faqAns.className = "hidden";
 
-document
-	.querySelector(".accordian_body .faq .faq_header .show_btn")
-	.addEventListener("click", () => {
-		if (faqAns.style.display == "flex") {
-			faqAns.style.display = "none";
-		} else {
-			faqAns.style.display = "flex";
-		}
-	});
+let faq = []; // Empty array for 3 different FAQs
+let faqHeader = []; //Empty array for 3 different FAQ Headers
+let faqQues = []; // Empty array for 3 different FAQ Questions
+let faqBtn = []; // Empty array for 3 different FAQ Buttons
+let faqAns = []; // Empty array for 3 different FAQ Answers
+
+// Loop to perform the required functionality for each FAQ
+for (let i = 0; i < 3; i = i + 1) {
+	//to create FAQ Section
+	faq.push(document.createElement("div"));
+	faq[i].className = "faq";
+	document.querySelector(".accordian_body").appendChild(faq[i]);
+	// to create FAQ Header Section
+	faqHeader.push(document.createElement("div"));
+	faqHeader[i].className = "faq_header";
+	document
+		.querySelectorAll(".accordian_body .faq")
+		[i].appendChild(faqHeader[i]);
+	// to create FAQ Question
+	faqQues.push(document.createElement("h3"));
+	document
+		.querySelectorAll(".accordian_body .faq .faq_header")
+		[i].appendChild(faqQues[i]);
+	faqQues[i].textContent = faqData[i].question;
+	// to create FAQ Button
+	faqBtn.push(document.createElement("button"));
+	faqBtn[i].className = "show_btn";
+	document
+		.querySelectorAll(".accordian_body .faq .faq_header")
+		[i].appendChild(faqBtn[i]);
+	faqBtn[i].textContent = "+";
+	// to create FAQ Answer
+	faqAns.push(document.createElement("p"));
+	document.querySelectorAll(".accordian_body .faq")[i].appendChild(faqAns[i]);
+	faqAns[i].textContent = faqData[i].answer;
+	faqAns[i].className = "hidden";
+	// to add click event listener on each FAQ Button to perform the required functionality
+	document
+		.querySelectorAll(".accordian_body .faq .faq_header .show_btn")
+		[i].addEventListener("click", () => {
+			if (faqAns[i].style.display == "flex") {
+				faqAns[i].style.display = "none";
+			} else {
+				faqAns[i].style.display = "flex";
+			}
+		});
+}
